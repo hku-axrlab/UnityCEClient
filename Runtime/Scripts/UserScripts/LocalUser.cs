03f3ea5e8b819ba751f6a6325bf9154cfff8dd3e
hku-axrlab <axrlab01@hku.nl>
2026-06-03 14:03:58 +0200

    Prevented user data from being empty when sent

--- Runtime/Scripts/UserScripts/LocalUser.cs
+++ Runtime/Scripts/UserScripts/LocalUser.cs
@@ -4,38 +4,38 @@ using UnityEngine;
 
 namespace UnityCEClient
 {
-	[Serializable]
-	public struct UserData
-	{
-		public string id;
-		public string name;
-		public List<string> boneNames;
-		public List<TransformData> boneTransforms;
-	}
+    [Serializable]
+    public struct UserData
+    {
+        public string id;
+        public string name;
+        public List<string> boneNames;
+        public List<TransformData> boneTransforms;
+    }
 
-	[Serializable]
-	public class Vector3Data
-	{
-		public float x, y, z;
+    [Serializable]
+    public class Vector3Data
+    {
+        public float x, y, z;
 
         public Vector3Data()
-		{
-			x = y = z = 0;
-		}
+        {
+            x = y = z = 0;
+        }
 
         public Vector3Data(Vector3 source)
-		{
-			x = source.x;
-			y = source.y;
-			z = source.z;
-		}
+        {
+            x = source.x;
+            y = source.y;
+            z = source.z;
+        }
 
-		public void Update(Vector3 source)
-		{
-			x = source.x;
-			y = source.y;
-			z = source.z;
-		}
+        public void Update(Vector3 source)
+        {
+            x = source.x;
+            y = source.y;
+            z = source.z;
+        }
 
         public static implicit operator Vector3(Vector3Data source)
         {
@@ -47,10 +47,10 @@ namespace UnityCEClient
         }
     }
 
-	[Serializable]
-	public class QuaternionData
-	{
-		public float x, y, z, w;
+    [Serializable]
+    public class QuaternionData
+    {
+        public float x, y, z, w;
 
         public QuaternionData()
         {
@@ -58,20 +58,20 @@ namespace UnityCEClient
         }
 
         public QuaternionData(Quaternion source)
-		{
-			x = source.x;
-			y = source.y;
-			z = source.z;
-			w = source.w;
-		}
+        {
+            x = source.x;
+            y = source.y;
+            z = source.z;
+            w = source.w;
+        }
 
-		public void Update(Quaternion source)
-		{
-			x = source.x;
-			y = source.y;
-			z = source.z;
-			w = source.w;
-		}
+        public void Update(Quaternion source)
+        {
+            x = source.x;
+            y = source.y;
+            z = source.z;
+            w = source.w;
+        }
 
         public static implicit operator Quaternion(QuaternionData source)
         {
@@ -79,7 +79,7 @@ namespace UnityCEClient
             r.x = source.x;
             r.y = source.y;
             r.z = source.z;
-			r.w	= source.w;
+            r.w = source.w;
             return r;
         }
 
@@ -94,137 +94,144 @@ namespace UnityCEClient
         }
     }
 
-	[Serializable]
-	public class TransformData
-	{
-		public Vector3Data position;
-		public QuaternionData rotation;
-		public Vector3Data scale;
+    [Serializable]
+    public class TransformData
+    {
+        public Vector3Data position;
+        public QuaternionData rotation;
+        public Vector3Data scale;
 
         public TransformData()
-		{
-			position = new Vector3Data();
-			rotation = new QuaternionData();
-			scale = new Vector3Data();
+        {
+            position = new Vector3Data();
+            rotation = new QuaternionData();
+            scale = new Vector3Data();
 
         }
 
         public static TransformData From(Transform t)
-		{
-			TransformData data = new TransformData();
+        {
+            TransformData data = new TransformData();
 
-			if (t != null)
-			{
-				data.position = new Vector3Data(t.position);
-				data.rotation = new QuaternionData(t.rotation);
-				data.scale = new Vector3Data(t.localScale);
-			}
+            if (t != null)
+            {
+                data.position = new Vector3Data(t.position);
+                data.rotation = new QuaternionData(t.rotation);
+                data.scale = new Vector3Data(t.localScale);
+            }
 
-			return data;
-		}
+            return data;
+        }
 
-		public void Update(Transform t)
-		{
-			if (t == null) return;
+        public void Update(Transform t)
+        {
+            if (t == null) return;
 
-			position.Update(t.position);
-			rotation.Update(t.rotation);
-			scale.Update(t.localScale);
-		}
-	}
+            position.Update(t.position);
+            rotation.Update(t.rotation);
+            scale.Update(t.localScale);
+        }
+    }
 
-	[Serializable]
-	struct UserMsg
-	{
-		public int msgType;
-		public UserData[] users;
+    [Serializable]
+    struct UserMsg
+    {
+        public int msgType;
+        public UserData[] users;
 
-		public UserMsg( LocalUser[] data )
-		{
-			msgType = (int)MessageType.ClientData;
-			users = new UserData[data.Length];
-			for( int i = 0; i < data.Length; ++i )
-			{
-				users[i] = data[i].GetUserData();
-			}
-		}
-	}
+        public UserMsg(LocalUser[] data)
+        {
+            msgType = (int)MessageType.ClientData;
+            users = new UserData[data.Length];
+            for (int i = 0; i < data.Length; ++i)
+            {
+                users[i] = data[i].GetUserData();
+            }
+        }
+    }
 
-	/// <summary>
-	/// Base class for any kind of user that needs to be sent.
-	/// Inherit from this class, and simply add your transforms to the list of bones.
-	/// Make sure they have the correct names that you want to send.
-	/// 
-	/// Name doesn't need to be unique, but can for instance represent the "kind of user" you want to display.
-	/// </summary>
-	public class LocalUser : MonoBehaviour
-	{
-		public const int USER_SEND_DELAY = 33;
+    /// <summary>
+    /// Base class for any kind of user that needs to be sent.
+    /// Inherit from this class, and simply add your transforms to the list of bones.
+    /// Make sure they have the correct names that you want to send.
+    /// 
+    /// Name doesn't need to be unique, but can for instance represent the "kind of user" you want to display.
+    /// </summary>
+    public class LocalUser : MonoBehaviour
+    {
+        public const int USER_SEND_DELAY = 33;
 
-		public string userName = "UnityUser";
-		public string id = "";
+        public string userName = "UnityUser";
+        public string id = "";
 
-		public List<Transform> bones = new List<Transform>();
+        public List<Transform> bones = new List<Transform>();
 
-		[Tooltip("Set this to make rotations relative to this object, for instance the initial-hips positions of a character")]
-		public Transform relativeToRotation = null;
+        [Tooltip("Set this to make rotations relative to this object, for instance the initial-hips positions of a character")]
+        public Transform relativeToRotation = null;
 
-		private UserData data = new UserData();
-		public UserData GetUserData()
-		{
-			return data;
-		}
+        private UserData data = new UserData();
+        public UserData GetUserData()
+        {
+            if (data.boneNames == null || data.boneTransforms == null)
+                UpdateData();
 
-		protected virtual void Start()
-		{
-			if (string.IsNullOrEmpty(id))
-				id = Utils.GenerateId();
-			else
-				id += "-" + Utils.GenerateId();
+            return data;
+        }
 
-			data.id = id;
+        protected virtual void Start()
+        {
+            if (string.IsNullOrEmpty(id))
+                id = Utils.GenerateId();
+            else
+                id += "-" + Utils.GenerateId();
+
+            data.id = id;
             data.name = userName;
 
-			if ( relativeToRotation != null )
-			{
-				// Create a Dummy transform with the initial rotation of the target
-				GameObject g = new GameObject("RelativeDummy");
-				g.transform.parent = transform;
-				g.transform.rotation = relativeToRotation.rotation;
-				g.transform.position = relativeToRotation.position;
-				relativeToRotation = g.transform;
+            if (relativeToRotation != null)
+            {
+                // Create a Dummy transform with the initial rotation of the target
+                GameObject g = new GameObject("RelativeDummy");
+                g.transform.parent = transform;
+                g.transform.rotation = relativeToRotation.rotation;
+                g.transform.position = relativeToRotation.position;
+                relativeToRotation = g.transform;
             }
 
             // Force a specific tag for now
             CalibrationEnvLinker.RegisterUser(this);
         }
 
-		protected virtual void LateUpdate()
-		{
-			// Update our user data
-			if (data.boneNames == null || data.boneTransforms == null)
-			{
-				data.boneNames = new List<string>(bones.Count);
-				data.boneTransforms = new List<TransformData>(bones.Count);
+        protected virtual void LateUpdate()
+        {
+            UpdateData();
+        }
 
-				foreach (Transform t in bones)
-				{
-					data.boneNames.Add(t.name);
-					data.boneTransforms.Add(TransformData.From(t));
-				}
-			}
-			else
-			{
-				// We assume the bone counts didn't change
-				for (int i = 0; i < bones.Count; ++i)
-				{
-					data.boneTransforms[i].Update(bones[i]);
-					if ( relativeToRotation )
-					{
+        private void UpdateData()
+        {
+            if (data.boneNames == null || data.boneTransforms == null)
+            {
+                data.boneNames = new List<string>(bones.Count);
+                data.boneTransforms = new List<TransformData>(bones.Count);
+
+                foreach (Transform t in bones)
+                {
+                    data.boneNames.Add(t.name);
+                    data.boneTransforms.Add(TransformData.From(t));
+                }
+            }
+            else
+            {
+                // We assume the bone counts didn't change
+                for (int i = 0; i < bones.Count; ++i)
+                {
+                    data.boneTransforms[i].Update(bones[i]);
+                    if (relativeToRotation)
+                    {
                         data.boneTransforms[i].rotation *= Quaternion.Inverse(relativeToRotation.rotation * Quaternion.Inverse(transform.rotation));
                     }
-				}
-			}
-		}
-	}
+                }
+            }
+        }
+    }
 }
