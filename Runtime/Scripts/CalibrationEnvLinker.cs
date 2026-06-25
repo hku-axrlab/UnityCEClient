a33a03eee5e424a793c1c73c4801d4c4c1128ef4
Rosa Corstjens <rosa.corstjens@gmail.com>
2026-06-25 14:20:16 +0100

    Created DataMonitor Editor class. Added readme.md to start dumping notes for later documentation. .

--- Runtime/Scripts/CalibrationEnvLinker.cs
+++ Runtime/Scripts/CalibrationEnvLinker.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.NetworkInformation;
-using System.Net.Sockets;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
